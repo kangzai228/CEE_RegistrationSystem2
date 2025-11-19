63d3585b65ec83ac5f51d2a93400cca5121e1d95
kangzilin <kangzai228@163.com>
2025-11-19 19:43:16 +0800

    2025年改版后的江西省高考报名系统

---
+++ queryBmRelatedRight.py
@@ -0,0 +1,34 @@
+import requests
+
+def queryBmRelatedRight(ticketNO,ksbmxxbh):
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        # 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        # 'Cache-Control': 'no-cache',
+        # 'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Origin': 'https://jxgk.jxeea.cn',
+        # 'Pragma': 'no-cache',
+        'Referer': 'https://jxgk.jxeea.cn/ksbm/cxtj/kscx/kscx',
+        # 'Sec-Fetch-Dest': 'empty',
+        # 'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0',
+        # 'X-Requested-With': 'XMLHttpRequest',
+        # 'sec-ch-ua': '"Chromium";v="142", "Microsoft Edge";v="142", "Not_A Brand";v="99"',
+        # 'sec-ch-ua-mobile': '?0',
+        # 'sec-ch-ua-platform': '"Windows"',
+        'ticketNO': ticketNO,
+    }
+
+    data = {
+        'queryType': '3',
+        'ksbmxxbh': ksbmxxbh,
+    }
+
+    response = requests.post(
+        'https://jxgk.jxeea.cn/ksy/ksbm/api/ksbm/ksbmxxData/queryBmRelatedRight.rest',
+        headers=headers,
+        data=data,
+    )
+    print(response.json())
---
+++ readme.md
@@ -0,0 +1,18 @@
+# 2025年改版后的江西省高考报名系统
+
+管理员网址 https://jxgk.jxeea.cn/admin/login
+
+
+## 1.新建虚拟环境
+
+```bash
+python3 -m venv ./.venv
+```
+
+## 2.激活虚拟环境
+
+```bash
+source ./.venv/bin/activate
+或者
+.venv\Scripts\activate.bat
+```
