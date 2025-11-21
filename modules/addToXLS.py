745ff42f5678e2a52574d402f03dd04297223829
kangzilin <kangzai228@163.com>
2025-11-21 13:29:21 +0800

    将数据添加到XLS文件,XLS的原数据不丢失

---
+++ modules/addToXLS.py
@@ -0,0 +1,41 @@
+# 将数据添加到XLS文件,XLS的原数据不丢失
+#coding:utf-8
+
+# xlrd==2.0.1
+# xlutils==2.0.0
+# xlwt==1.3.0
+
+# file_path:文件名(可以包括路径)
+# oneRowData:一行的数据
+# rowNum:行号
+from xlutils.copy import copy
+import xlrd,xlwt,os
+def addToXLS(file_path,oneRowData,rowNum):
+    if not os.exists(file_path):
+        workbook = xlwt.Workbook(encoding='utf-8')
+        sheet = workbook.add_sheet('Sheet1')
+        workbook.save(file_path)
+        
+    # 打开想要更改的excel文件
+    excel_temp = xlrd.open_workbook(file_path, formatting_info=True)
+    sheet1 = excel_temp.sheets()[0] # 打开第一张表
+    nrows = sheet1.nrows #总行数
+    ncols = sheet1.ncols #总列数
+    print("总行数：",nrows)
+    print("总列数：",ncols)
+    # 将操作文件对象拷贝，变成可写的workbook对象
+    new_excel = copy(excel_temp)
+    # 获得第一个sheet的对象
+    ws = new_excel.get_sheet(0)
+    ncols=len(oneRowData)
+    for col in range(ncols):
+        ws.write(rowNum, col, oneRowData[col]) #单元格A1开始，即(0,0)
+        # break
+    # 另存为excel文件，并将文件命名，可以重新命名，应该也可以覆盖掉
+    new_excel.save(file_path)
+    # print("数据已经保存到xls文件")
+
+if __name__ == '__main__':
+    file_path = "./stu_info.xls"
+    oneRowData = [1,2,3,4,5,6,7,8,9,10]
+    addToXLS(file_path,oneRowData,0)
